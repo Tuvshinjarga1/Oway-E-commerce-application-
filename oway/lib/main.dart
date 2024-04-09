710f1ea22534caa8de4484110ded5ccc108df917
Tuvshinjarga1 <ttuvshuu40@yahoo.com>
2024-04-09 12:13:23 +0800

    auth

--- oway/lib/main.dart
+++ oway/lib/main.dart
@@ -1,67 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:oway/turul.dart';
+import 'package:oway/home.dart';
+import 'package:oway/phone_otp_ui/phone.dart';
+import 'package:oway/phone_otp_ui/verify.dart';
 
-void main() {
+void main() async 
+{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyCeKQDpM26-BlTStCTFCZi8QHE9kJenIxA",
+      appId: "AIzaSyCeKQDpM26-BlTStCTFCZi8QHE9kJenIxA",
+      messagingSenderId: "AIzaSyCeKQDpM26-BlTStCTFCZi8QHE9kJenIxA",
+      projectId: "oway-cc924",
+    ),
+  );
   runApp(MaterialApp(
-    title: "Oway",
-    home: MyApp(),
+    initialRoute: 'phone',
+    debugShowCheckedModeBanner: false,
+    routes: {
+      'phone': (context) => MyPhone(),
+      'verify': (context) => MyVerify(),
+      'home': (context) => HomePage(),
+    },
   ));
-}
-
-class MyApp extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(title: Image.asset(
-        'assets/oway.png',
-        height: 100,
-        width: 100,
-      ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Align(
-              alignment: FractionalOffset(0.2, 0.5),
-              child: Text("Өрхийн",
-              style: TextStyle(fontSize: 22),
-              ),
-              ),
-              Align(
-              alignment: FractionalOffset(0.5, 0.5),
-              child: Text("үйлдвэрлэлээс",
-              style: TextStyle(fontSize: 22),
-              ),
-              ),
-            Image.asset(
-              'assets/ehleh.png',
-              width: 350,
-              height: 350,
-            ),
-            Align(
-              alignment: FractionalOffset(0.8, 0.5),
-              child: Text("таны гарт",
-              style: TextStyle(fontSize: 22)),
-              ),
-            SizedBox(height: 20,),
-            Align(
-              alignment: FractionalOffset(0.5, 1.0),
-              child: ElevatedButton(
-                child: Text("Эхлэх",
-              style: TextStyle(fontSize: 21,fontWeight: FontWeight.bold)),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.cyan),
-                  foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                ),
-                onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => Ouei()));
-                },
-              ),
-            )
-          ],
-        )
-      ),
-    );
-  }
 }
